9055131e558e6a196bbe5f36f66fb023e4a60be6
baruzdinasofya <150126434+baruzdinasofya@users.noreply.github.com>
2024-04-25 22:28:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-print('Привет! Надеюсь, ты ознакомился с правилами игры :), введи количество поколений:')
-generation = int(input())   #количество поколений
-print('Введите строку:')
-str0 = list(input())    #вводится строка
-last_index = len(str0) - 1  #последний индекс строки
-count = 0   #счетчик живых клеток
-while generation != 0 or count == len(str0):
-    for i in range(len(str0)):
-        if i == 0:
-            if str0[last_index] == '#' and str0[i + 1] == '#':
-                str0[i] = '_'
-            if str0[last_index] == '_' and str0[i + 1] == '_':
-                str0[i] = '_'
-            if str0[last_index] == '_' and str0[i + 1] == '#':
-                str0[i] = '#'
-            if str0[last_index] == '#' and str0[i + 1] == '_':
-                str0[i] = '#'
-
-        if i == last_index:
-            if str0[i - 1] == '#' and str0[0] == '#':
-                str0[i] = '_'
-            if str0[i - 1] == '_' and str0[0] == '_':
-                str0[i] = '_'
-            if str0[i - 1] == '_' and str0[0] == '#':
-                str0[i] = '#'
-            if str0[i - 1] == '#' and str0[0] == '_':
-                str0[i] = '#'
-
-        else:
-            if str0[i - 1] == '#' and str0[i + 1] == '#':
-                str0[i] = '_'
-            if str0[i - 1] == '_' and str0[i + 1] == '_':
-                str0[i] = '_'
-            if str0[i - 1] == '_' and str0[i + 1] == '#':
-                str0[i] = '#'
-            if str0[i - 1] == '#' and str0[i + 1] == '_':
-                str0[i] = '#'
-        for k in range(len(str0)):  #проверка, остались ли живые клетки
-            if str0[k] == '_':
-                count = count + 1
-        if count == len(str0):
-            print('Упс! Живых клеток не осталось :(')
-            print('(для окончания программы введите любой символ)')
-            input()
-            exit()
-    generation = generation - 1
-    print(*str0)
-
-if count != len(str0):
-    print('Игра окончена, остались живые клетки!')
-    print('(для окончания программы введите любой символ)')
-    input()
+print('Привет! Надеюсь ты ознакомился с правилами игры, Начнем.')
+const = '1'
+while const == '1':
+    print('Введите длину мира:')
+    len_world = int(input())
+    list_word = list(len_world * ' ')
+    list2 = list(len_world * ' ')
+    print('Введите номера ячеек, в которых вы бы хотели зародить жизнь:')
+    number = input().split()
+    for i in number:
+        list_word[int(i) - 1] = '#'
+        list2[int(i) - 1] = '#'
+    print('Введите количество поколений:')
+    a = int(input())
+    print('0)' + ''.join(list_word))
+    for j in range(a):
+        for i in range(len(list_word)):
+            if i != len(list_word) - 1:
+                if list_word[i - 1] == '#' and list_word[i + 1] == '#':
+                    list2[i] = ' '
+                elif list_word[i - 1] == ' ' and list_word[i + 1] == ' ':
+                    list2[i] = ' '
+                elif list_word[i - 1] == '#' and list_word[i + 1] == ' ':
+                    list2[i] = '#'
+                elif list_word[i - 1] == ' ' and list_word[i + 1] == '#':
+                    list2[i] = '#'
+            elif i == len(list_word) - 1:
+                if list_word[i - 1] == '#' and list_word[0] == '#':
+                    list2[i] = ' '
+                elif list_word[i - 1] == ' ' and list_word[0] == ' ':
+                    list2[i] = ' '
+                elif list_word[i - 1] == '#' and list_word[0] == ' ':
+                    list2[i] = '#'
+                elif list_word[i - 1] == ' ' and list_word[0] == '#':
+                    list2[i] = '#'
+        print(str(j + 1) + ')' + ''.join(list2))
+        for i in range(len(list2)):
+            list_word[i] = list2[i]
+    print('Хотите продолжить играть:')
+    print('1) да')
+    print('2) нет')
+    const = input()
+print('До встречи! (для завершения программы введите любой символ)')
+input()
